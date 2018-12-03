c8a19d6eecf361d8a2f7a310e70437e8c77a2a68
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2018-12-03 22:54:21 +0700

    Initial Project

---
+++ src/main/java/com/rizaldi/steam/web/SteamWebApplication.java
@@ -0,0 +1,12 @@
+package com.rizaldi.steam.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamWebApplication.class, args);
+    }
+}
---
+++ src/test/java/com/rizaldi/steam/web/SteamWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rizaldi.steam.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SteamWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
